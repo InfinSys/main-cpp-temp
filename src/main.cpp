14981d3b65bada2d53cdbc89e3434d95541926ef
Jamon Bailey <83630529+ViRtigo23@users.noreply.github.com>
2024-12-14 12:43:59 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,17 @@
 
 #include<iostream>
+#include"meta/info.h"
+#include"meta/version.h"
 
 
 int main(int argc, char* argv[])
 {
     //
-    std::cout << "\nFunctioning properly." << std::endl;
+    std::cout << "\nFunctioning properly."
+        << "\nTitle: " << TEMPLATEMetadata::OFFICIAL_NAME
+        << "\nShort name: " << TEMPLATEMetadata::SHORT_NAME
+        << "\nPublisher: " << TEMPLATEMetadata::PUBLISHER << '\n'
+        << std::endl;
 
     return 0;
 }
