164075397445ca467413abe593f0ceeb658545e8
Jamon Bailey <83630529+ViRtigo23@users.noreply.github.com>
2024-12-14 11:07:01 -0500

    Generate version.h

---
+++ include/meta/version.h
@@ -0,0 +1,23 @@
+
+/*!
+* @file version.h
+*
+* @brief
+* <SHORT_NAME> version document.
+*
+* @details
+* Version information for <SOFTWARE_FULL_NAME>.
+*/
+
+
+
+#ifndef TEMPLATE_METADATA_VERSION_STRUCT_H_
+#define TEMPLATE_METADATA_VERSION_STRUCT_H_
+
+
+
+//
+
+
+
+#endif // TEMPLATE_METADATA_VERSION_STRUCT_H_
